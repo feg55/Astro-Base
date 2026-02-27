22583eeaba31a2ee8253c99137079a3f0cca8adb
feg55 <andhkj@gmail.com>
2026-02-27 14:36:24 +0300

    1

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import RenderedSphere from './Sphere'
+
+function App() {
+  return (
+    <main>
+      <h1>Astro Base</h1>
+      <RenderedSphere />
+    </main>
+  )
+}
+
+export default App
---
+++ src/Sphere.tsx
@@ -0,0 +1,47 @@
+import { Canvas, useFrame, type ThreeElements, type ThreeEvent } from '@react-three/fiber'
+import { useRef, useState } from 'react'
+import type { Mesh } from 'three'
+
+function SphereMesh(props: ThreeElements['mesh']) {
+  const meshRef = useRef<Mesh | null>(null)
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  useFrame((_state, delta: number) => {
+    if (meshRef.current) meshRef.current.rotation.y += delta
+  })
+
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 3}
+      onClick={(event: ThreeEvent<MouseEvent>) => {
+        event.stopPropagation()
+        setActive(!active)
+      }}
+      onPointerOver={(event: ThreeEvent<PointerEvent>) => {
+        event.stopPropagation()
+        setHover(true)
+      }}
+      onPointerOut={(event: ThreeEvent<PointerEvent>) => {
+        event.stopPropagation()
+        setHover(false)
+      }}
+    >
+      <sphereGeometry />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+export default function RenderedSphere() {
+  return (
+    <Canvas style={{ width: '100%', height: '400px' }}>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <SphereMesh position={[0, 0, 0]} />
+    </Canvas>
+  )
+}
